a4b7886475e9fdaacb9960e3064219b7f759404f
DavidCaneiro <davidcaneiro@outlook.com>
2017-11-24 12:10:51 +0100

    menu

--- juego.java
+++ juego.java
@@ -2,9 +2,33 @@
 public class juego {
     
         public static void main(String[] args) {
-            // TODO Auto-generated method stub
-    
+            int sacar = 0;
+            System.out.println("¿Que quieres sacar?");
+            System.out.println( "Pulsa 1 para Tijeras");
+            System.out.println( "Pulsa 2 para Piedra");
+            System.out.println( "Pulsa 3 para Pepel");
+            System.out.println( "Pulsa 4 para Salir");
+            Scanner sc = new Scanner(System.in);
+            sacar = sc.nextInt();
+            switch(sacar) {
+            case 1: 
+                System.out.println("Tijera");
+                break;
+            case 2:
+                System.out.println("Piedra");
+                break;
+            case 3:
+                System.out.println("Papel");
+                break;
+            case 4:
+                System.out.println("Hasta pronto");
+                break;
+            default:
+                break;
+                
+            }
+            sc.close();
         }
+      }
     
-    }
     
