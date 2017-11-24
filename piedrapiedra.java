0d8612f80e932538d08422441f9ceeedd219e22f
daniel <daniel@daniels-MacBook-Pro.local>
2017-11-24 12:40:39 +0100

    ddd

---
+++ piedrapiedra.java
@@ -0,0 +1,11 @@
+
+            // Ver si ha salido piedra-piedra y mostrar el mensaje
+        String piedra;
+
+        Scanner sc = new Scanner(System.in);
+        piedra = sc.nextInt();
+        if (piedra == piedra)
+
+            System.out.println("Es un empate");
+
+    
