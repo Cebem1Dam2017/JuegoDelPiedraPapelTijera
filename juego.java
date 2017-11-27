8e7184b2ca1020b9ea9c988b5f1b373795b28abf
J3DM <josemdmarcos@gmail.com>
2017-11-27 12:37:55 +0100

    Añadido código de empate mas método que devuelve jugada en ambos idiomas

--- juego.java
+++ juego.java
@@ -1,11 +1,8 @@
 
-import java.util.Scanner;
-
-
 public class juego {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
         int sacar = 0;
         int idioma = 0;
 
@@ -19,6 +16,7 @@ public class juego {
             case 2:
                 idioma = 2;
         }
+        int jugagaOrdenador=(int)(Math.random()*3)+1;      
 
         if (idioma == 1) {
             System.out.println("¿Que quieres sacar?");
@@ -28,49 +26,93 @@ public class juego {
             System.out.println("Pulsa 4 para Salir");
 
             sacar = sc.nextInt();
-            switch (sacar) {
-                case 1:
-                    System.out.println("Tijera");
-                    break;
-                case 2:
-                    System.out.println("Piedra");
-                    break;
-                case 3:
-                    System.out.println("Papel");
-                    break;
-                case 4:
-                    System.out.println("¡Hasta pronto!");
-                    break;
-                default:
-                    break;
-            }
-        }
-        if (idioma == 2) {
-            System.out.println("¿Que quieres sacar?");
-            System.out.println("Pulsa 1 para Tijeras");
-            System.out.println("Pulsa 2 para Piedra");
-            System.out.println("Pulsa 3 para Pepel");
-            System.out.println("Pulsa 4 para Salir");
 
-            sacar = sc.nextInt();
-            switch (sacar) {
+            if(sacar==jugagaOrdenador && sacar!=4){//EMPATE
+                System.out.print("Empate "+dimeJugada(sacar));
+            }else{//No empate
+                switch (sacar) {
+                    case 1:
+                        System.out.println("Tijera");
+                        break;
+                    case 2:
+                        System.out.println("Piedra");
+                        break;
+                    case 3:
+                        System.out.println("Papel");
+                        break;
+                    case 4:
+                        System.out.println("¡Hasta pronto!");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }else if(idioma==2){
+                System.out.println("¿Que quieres sacar?");
+                System.out.println("Pulsa 1 para Tijeras");
+                System.out.println("Pulsa 2 para Piedra");
+                System.out.println("Pulsa 3 para Pepel");
+                System.out.println("Pulsa 4 para Salir");
+
+                sacar = sc.nextInt();
+
+                if(sacar==jugagaOrdenador && sacar!=4){//EMPATE
+                    System.out.print("Empate "+dimeJugada(sacar));
+                }else{//No empate
+                    switch (sacar) {
+                        case 1:
+                            System.out.println("Scissors");
+                            break;
+                        case 2:
+                            System.out.println("Stone");
+                            break;
+                        case 3:
+                            System.out.println("Paper");
+                            break;
+                        case 4:
+                            System.out.println("See you soon!");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+        }
+        sc.close();
+        
+    }
+    public static String dimeJugada(int jug){
+        string ret="";
+        if(idioma==0){
+            switch (jug) {
                 case 1:
-                    System.out.println("Scissors");
+                    ret="Tijera";
                     break;
                 case 2:
-                    System.out.println("Stone");
+                    ret="Piedra";
                     break;
                 case 3:
-                    System.out.println("Paper");
-                    break;
-                case 4:
-                    System.out.println("See you soon!");
+                    ret="Papel";
                     break;
                 default:
+                    ret="error";
                     break;
             }
-            sc.close();
+        }else if(idioma==1){
+            switch (sacar) {
+                case 1:
+                    ret="Scissors";
+                    break;
+                case 2:
+                    ret="Stone";
+                    break;
+                case 3:
+                    ret="Paper";
+                    break;
+                default:
+                    ret="error";
+                    break;
+            }
         }
+        return ret;
     }
 }
-
